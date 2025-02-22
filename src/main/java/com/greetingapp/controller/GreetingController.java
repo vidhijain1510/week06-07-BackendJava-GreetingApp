573c51e6e1fa430c68cc021c2b42a766814d43c5
Vidhi Jain <vidhijain507@gmail.com>
2025-02-22 12:11:57 +0530

    [Vidhi_Jain]Add. Greeting controller returning JSON for different HTTP Methods using service Layer.

--- src/main/java/com/greetingapp/controller/GreetingController.java
+++ src/main/java/com/greetingapp/controller/GreetingController.java
@@ -1,11 +1,15 @@
 package com.greetingapp.controller;
 
+import com.greetingapp.service.GreetingService;
 import com.greetingapp.model.Greeting;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/api")
 public class GreetingController {
+
+    //UC1
     @GetMapping("/greet")
     public Greeting getGreeting() {
         return new Greeting("Hello from BridgeLabz");
@@ -24,5 +28,19 @@ public class GreetingController {
     @DeleteMapping("/greet")
     public Greeting deleteGreeting() {
         return new Greeting("Greeting deleted");
+    }
+
+
+    //UC2
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greetservice")
+    public Greeting getGreetings() {
+        return new Greeting(greetingService.getGreetingMessage());
     }
 }
---
+++ src/main/java/com/greetingapp/service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.greetingapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String getGreetingMessage() {
+        return "Hello World";
+    }
+}
