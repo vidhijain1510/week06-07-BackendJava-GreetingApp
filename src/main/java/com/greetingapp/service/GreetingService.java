4b42a011e6ce44ac7a1c68bb472b28f085bcd91b
Vidhi Jain <vidhijain507@gmail.com>
2025-02-22 16:16:34 +0530

    [Vidhi_Jain]Add. Greeting controller returning JSON for different HTTP Methods using service Layer be taking inputs.

--- src/main/java/com/greetingapp/service/GreetingService.java
+++ src/main/java/com/greetingapp/service/GreetingService.java
@@ -4,7 +4,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-    public String getGreetingMessage() {
+    /*public String getGreetingMessage() {
         return "Hello World";
+    }*/
+
+    public String getGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName;
+        } else if (firstName != null) {
+            return "Hello " + firstName;
+        } else if (lastName != null) {
+            return "Hello " + lastName;
+        } else {
+            return "Hello World";
+        }
     }
 }
